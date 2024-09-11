f57bd80c91885ea22f4df5daf14d7ad69febdc66
kevas cole <colekevas@gmail.com>
2024-09-11 18:43:39 +0100

    added 3 nav pages

---
+++ src/components/Compare.tsx
@@ -0,0 +1,6 @@
+const Compare = () => {
+    return(
+       <p> compare</p>
+    )
+}
+export default Compare;
---
+++ src/components/Homepage.tsx
@@ -0,0 +1,12 @@
+const Homepage = () => {
+  return (
+    <div className="container h-80 mx-auto my-4 p-4 border-4 border-yellow-400">
+      main container
+      <div className="container h-40 mx-auto my-4 p-4 border-4 border-red-400">
+        lihgg
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
