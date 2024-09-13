9ca67b185b42c5fab24ee9db2050885ef4eb23aa
kevas cole <colekevas@gmail.com>
2024-09-13 17:08:06 +0100

    added picture, changed background colour to match picture and aligned all text as i want it

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+import Shelfie from "../app/img/Shelfie.jpg";
+
 const Homepage = () => {
   return (
-    <div className="container h-80 mx-auto my-4 p-4 border-4 border-yellow-400">
-      main container
-      <div className="container h-40 mx-auto my-4 p-4 border-4 border-red-400">
-        lihgg
+    <div>
+      <div className="container flex flex-col items-center justify-center mt-52 h-80 mx-auto my-4 p-4 ">
+        <h1 className="font-bebas text-9xl font-bold tracking-widest">
+          BEAUTY
+        </h1>
+        <p className="font-bebas text-9xl font-bold tracking-widest">DECODER</p>
+
+        <Image src={Shelfie} alt="Shelfie with no brands" />
+        {/* <p className="font-bebas text-2xl font-normal flex items-center m-2 justify-center ">
+          A simple skincare ingredients comparison website
+        </p> */}
       </div>
     </div>
   );
