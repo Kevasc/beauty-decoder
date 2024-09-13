20fe39f232a990d99dec657bcfb506889f8f61e8
kevas cole <colekevas@gmail.com>
2024-09-13 17:07:13 +0100

    imported main h1 font from google

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        bebas: ["Bebas Neue", "sans-serif"],
+      },
     },
   },
   plugins: [],
