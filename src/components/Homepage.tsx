5a1f821919c265ace99c94857ff18593b691850d
kevas cole <colekevas@gmail.com>
2024-10-10 11:54:37 +0100

    rewrote description and reduced margin sizing

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,13 +4,11 @@ import Shelfie from "../app/img/Shelfie.jpg";
 const Homepage = () => {
   return (
     <div>
-      <div className="container flex flex-col items-center justify-center mt-52 h-80 mx-auto my-4 p-4 ">
-        <h1 className="font-bebas text-9xl font-bold tracking-widest mt-9">
-          BEAUTY
-        </h1>
-        <p className="font-bebas text-9xl font-bold tracking-widest">DECODER</p>
-        <p className="font-bebas text-2xl font-normal flex items-center m-2 mt-4 mb-9 justify-center ">
-          A simple skincare ingredients comparison website
+      <div className="container flex flex-col items-center justify-center mx-auto">
+        <h1 className="font-bebas text-9xl font-bold  mt-5">BEAUTY</h1>
+        <p className="font-bebas text-9xl font-bold ">DECODER</p>
+        <p className="font-bebas text-2xl font-normal flex items-center m-2 mt-4  justify-center ">
+          A simple cosmetics ingredients comparison website
         </p>
         <Image src={Shelfie} alt="Shelfie with no brands" />
       </div>
