f479411d01cfd6138a0987d6d5ee6ef540a35d08
kevas cole <colekevas@gmail.com>
2024-10-11 09:56:43 +0100

    moved homepage to this file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import BurgerMenu from "@/components/BurgerMenu";
-import Homepage from "@/components/Homepage";
+import Image from "next/image";
+import Shelfie from "../app/img/Shelfie.jpg";
 
-export default function Home() {
+const Homepage = () => {
   return (
     <div>
-      <BurgerMenu />
-      <Homepage />
+      <div className="container flex flex-col items-center justify-center mx-auto">
+        <h1 className="font-bebas text-9xl font-bold  mt-5">BEAUTY</h1>
+        <p className="font-bebas text-9xl font-bold ">DECODER</p>
+        <p className="font-bebas text-2xl font-normal flex items-center m-2 mt-4  justify-center ">
+          A simple cosmetics ingredients comparison website
+        </p>
+        <Image src={Shelfie} alt="Shelfie with no brands" />
+      </div>
     </div>
   );
-}
+};
+
+export default Homepage;
