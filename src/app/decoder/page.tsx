74da5f3d1427765eb0719792613b3c46c82fd53b
kevas cole <colekevas@gmail.com>
2024-10-11 18:05:08 +0100

    created an array of beauty products and displayed the selection of products onto cards

--- src/app/decoder/page.tsx
+++ src/app/decoder/page.tsx
@@ -1,13 +1,40 @@
+"use client";
+
+const productTypeArray = [
+  "Blush",
+  "Bronzer",
+  "Eyebrow",
+  "Eyeliner",
+  "Eyeshadow",
+  "Foundation",
+  "Lip Liner",
+  "Lipstick",
+  "Mascara",
+  "Nail Polish",
+];
+
 const Decoder = () => {
+  const productCards = productTypeArray.map((product, index) => {
+    return (
+      <div
+        key={index}
+        className="shadow-lg rounded transition-transform transform hover:scale-105"
+      >
+        {/* <img className="w-full h-48 object-cover rounded-t" src={`/img/${product.toLowerCase().replace(" ", "-")}.jpg`} alt={`${product}`} /> */}
+        <div className="px-6 py-10 flex justify-center ">
+          <p className="font-bold text-xl ">{product}</p>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="container flex flex-col items-center justify-center mx-auto">
         <h1 className="font-bebas text-3xl font-bold  mt-5">BEAUTY</h1>
         <p className="font-bebas text-3xl font-bold ">DECODER</p>
-        <p className="font-bebas text-2xl font-normal flex items-center m-2 mt-4  justify-center ">
-          Search for your product here
-        </p>
       </div>
+      <div className="grid grid-cols-5  gap-10 p-10">{productCards}</div>
     </div>
   );
 };
