f73f9cc0d254026dbb86a88fec8deca54e6fa655
kevas cole <colekevas@gmail.com>
2024-10-11 10:30:37 +0100

    added styling to about and decoder pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,15 @@
 const About = () => {
-    return(
-       <p> about</p>
-    )
-}
+  return (
+    <div>
+      <div className="container flex flex-col items-center justify-center mx-auto">
+        <h1 className="font-bebas text-3xl font-bold  mt-5">BEAUTY</h1>
+        <p className="font-bebas text-3xl font-bold ">DECODER</p>
+        <p className="font-bebas text-2xl font-normal flex items-center m-2 mt-4  justify-center ">
+          A simple way to check if the cosmetics you are using are vegan, gluten
+          free and crulety free.
+        </p>
+      </div>
+    </div>
+  );
+};
 export default About;
--- src/app/decoder/page.tsx
+++ src/app/decoder/page.tsx
@@ -1,8 +1,14 @@
 const Decoder = () => {
   return (
-    <>
-      <p>decoder</p>
-    </>
+    <div>
+      <div className="container flex flex-col items-center justify-center mx-auto">
+        <h1 className="font-bebas text-3xl font-bold  mt-5">BEAUTY</h1>
+        <p className="font-bebas text-3xl font-bold ">DECODER</p>
+        <p className="font-bebas text-2xl font-normal flex items-center m-2 mt-4  justify-center ">
+          Search for your product here
+        </p>
+      </div>
+    </div>
   );
 };
 export default Decoder;
