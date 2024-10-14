14f8a16fce5bc0bf1d0154b49da0b81b07aee8f0
kevas cole <colekevas@gmail.com>
2024-10-14 18:16:22 +0100

    assigned each card to a different color,

--- src/app/decoder/page.tsx
+++ src/app/decoder/page.tsx
@@ -13,16 +13,16 @@ import Mascara from "../img/Mascara.png";
 import NailPolish from "../img/NailPolish.png";
 
 const productTypeArray = [
-  { name: "Blush", img: Blush },
-  { name: "Bronzer", img: Bronzer },
-  { name: "Eyebrow", img: Eyebrow },
-  { name: "Eyeliner", img: Eyeliner },
-  { name: "Eyeshadow", img: Eyeshadow },
-  { name: "Foundation", img: Foundation },
-  { name: "Lip Liner", img: LipLiner },
-  { name: "Lipstick", img: Lipstick },
-  { name: "Mascara", img: Mascara },
-  { name: "Nail Polish", img: NailPolish },
+  { name: "Blush", img: Blush, color: "#F58F29" },
+  { name: "Bronzer", img: Bronzer, color: "#712f79" },
+  { name: "Eyebrow", img: Eyebrow, color: "#4C4C9D" },
+  { name: "Eyeliner", img: Eyeliner, color: "#5BC0BE" },
+  { name: "Eyeshadow", img: Eyeshadow, color: "#EE4266" },
+  { name: "Foundation", img: Foundation, color: "#EE4266" },
+  { name: "Lip Liner", img: LipLiner, color: "#5BC0BE" },
+  { name: "Lipstick", img: Lipstick, color: "#4C4C9D" },
+  { name: "Mascara", img: Mascara, color: "#712f79" },
+  { name: "Nail Polish", img: NailPolish, color: "#F58F29" },
 ];
 
 const Decoder = () => {
@@ -30,13 +30,14 @@ const Decoder = () => {
     return (
       <div
         key={index}
-        className="shadow-lg rounded transition-transform transform hover:scale-105 bg-pink-300"
+        className="shadow-lg rounded transition-transform transform hover:scale-105"
+        style={{ backgroundColor: product.color }}
       >
         <div className="bg-white p-3 flex rounded justify-center">
           <Image width={100} src={product.img} alt={product.name} />
         </div>
         <div className="py-5 flex justify-center ">
-          <p className="font-bold text-2xl ">{product.name}</p>
+          <p className="font-bebas text-2xl ">{product.name}</p>
         </div>
       </div>
     );
@@ -48,7 +49,7 @@ const Decoder = () => {
         <h1 className="font-bebas text-3xl font-bold  mt-5">BEAUTY</h1>
         <p className="font-bebas text-3xl font-bold ">DECODER</p>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 p-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 p-10 ">
         {productCards}
       </div>
     </div>
