bda218c89292391e1b700ceaddd8ef43f5b671a5
kevas cole <colekevas@gmail.com>
2024-10-14 16:12:56 +0100

    added breakpoints

--- src/app/decoder/page.tsx
+++ src/app/decoder/page.tsx
@@ -48,7 +48,9 @@ const Decoder = () => {
         <h1 className="font-bebas text-3xl font-bold  mt-5">BEAUTY</h1>
         <p className="font-bebas text-3xl font-bold ">DECODER</p>
       </div>
-      <div className="grid grid-cols-5  gap-10 p-10">{productCards}</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 p-10">
+        {productCards}
+      </div>
     </div>
   );
 };
