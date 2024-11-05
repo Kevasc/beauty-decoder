f1332d481b475be3c5ce75a09902f832bf0b86fe
kevas cole <colekevas@gmail.com>
2024-11-05 22:39:15 +0000

    created types for productDetail and added product to end of api call

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,9 @@
-export const getProducts = async () => {
+export const getProducts = async (
+  product: string
+): Promise<ProductDetail[] | undefined> => {
   try {
     const response = await fetch(
-      "http://makeup-api.herokuapp.com/api/v1/products.json",
+      `http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product}`,
       {
         method: "GET",
       }
@@ -18,7 +20,24 @@ export const getProducts = async () => {
   }
 };
 
-// Log the data after calling the function
-getProducts()
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Error:", error));
+export type ProductDetail = {
+  api_featured_image: string;
+  brand: string;
+  category: null | string;
+  created_at: string;
+  currency: null | string;
+  description: string;
+  id: number;
+  image_link: string;
+  name: string;
+  price: string;
+  price_sign: null | string;
+  product_api_url: string;
+  product_colors: { hex_value: string; colour_name: string }[];
+  product_link: string;
+  product_type: string;
+  rating: number;
+  tag_list: [];
+  updated_at: string;
+  website_link: string;
+};
