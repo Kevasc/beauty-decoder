cd6838f1373cbf7429d22df550d9c865c74602b5
kevas cole <colekevas@gmail.com>
2024-11-05 22:36:29 +0000

    added contents of ProductCard to own file for easy access and reducing file size

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,30 @@
+import { Product } from "@/app/decoder/page";
+import Image from "next/image";
+
+interface ProductCardProps {
+  product: Product;
+  onClick: () => void;
+}
+
+export const ProductCard = ({ product, onClick }: ProductCardProps) => {
+  return (
+    <div
+      className="shadow-lg transition-transform transform hover:scale-105"
+      style={{ backgroundColor: product.color }}
+      onClick={onClick}
+    >
+      <div className="bg-white p-3 flex justify-center">
+        {product.img ? (
+          <Image width={100} src={product.img} alt={product.name} />
+        ) : (
+          <div className="text-center font-bold text-black-500">
+            {product.name}
+          </div>
+        )}
+      </div>
+      <div className="py-5 flex justify-center">
+        <p className="font-mono text-2xl">{product.name}</p>
+      </div>
+    </div>
+  );
+};
