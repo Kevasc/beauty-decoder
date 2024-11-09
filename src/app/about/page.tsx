752ab6dea660d0bb93f55e8d8ddfba37d3c8ae10
kevas cole <colekevas@gmail.com>
2024-11-09 12:13:22 +0000

    took off purple background as it is now in the global client.layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,11 @@
 const About: React.FC = () => {
   return (
-    <div>
-      <div className="container flex flex-col items-center justify-center mx-auto bg-purple-50">
-        <h1 className="font-bebas text-3xl font-bold  mt-5">BEAUTY DECODER</h1>
-        <p className="font-bebas text-2xl font-normal flex items-center m-2 mt-4  justify-center ">
-          A simple way to check if the cosmetics you are using are vegan, gluten
-          free and crulety free.
-        </p>
-      </div>
+    <div className="container flex flex-col items-center justify-center mx-auto ">
+      <h1 className="font-bebas text-3xl font-bold  mt-5">BEAUTY DECODER</h1>
+      <p className="font-bebas text-2xl font-normal flex items-center m-2 mt-4  justify-center ">
+        A simple way to check if the cosmetics you are using are vegan, gluten
+        free and crulety free.
+      </p>
     </div>
   );
 };
