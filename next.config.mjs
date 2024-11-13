843875b7902e1624793f1b802d5efd453fdd305f
kevas cole <colekevas@gmail.com>
2024-11-13 22:43:17 +0000

    deleted as now using img tag in the DetailsCard component

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  images: {
-    domains: ["d3t32hsnjxo7q6.cloudfront.net"], // Add the required external domains here
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
