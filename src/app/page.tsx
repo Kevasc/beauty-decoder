e09e2a2206d8e81b53a111b61a460b94a89886eb
kevas cole <colekevas@gmail.com>
2024-12-19 11:30:22 +0000

    imported and added typewriter effect to homepage description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,38 @@
-import Image from "next/image";
-import MakeupProducts from "./img/MakeupProducts.png";
+"use client";
+import Typewriter from "typewriter-effect";
 
 const Homepage = () => {
   return (
     <div
       data-testid="homepage_container"
-      className="min-h-screen flex flex-col items-center justify-center "
+      className="min-h-screen flex flex-col items-center justify-center"
     >
       <div className="container flex flex-col items-center justify-center mx-auto">
         <h1
           data-testid="heading_top_line"
-          className="font-bebas text-9xl text-fuchsia-900 font-bold mt-5"
+          className="font-bebas text-9xl text-fuchsia-900 font-bold"
         >
           BEAUTY
         </h1>
         <p
           data-testid="heading_bottom_line"
-          className="font-bebas text-9xl text-fuchsia-900 font-bold "
+          className="font-bebas text-9xl text-fuchsia-900 font-bold"
         >
           DECODER
         </p>
-        <p
-          data-testid="homepage_description"
-          className="font-mono text-2xl font-normal text-neutral-900 flex items-center m-2 mt-4  justify-center "
-        >
-          A simple cosmetics ingredients comparison website
-        </p>
-        <Image
-          data-testid="main_image"
-          src={MakeupProducts}
-          alt="Makeup smear"
-        />
+
+        <div className="font-mono text-2xl font-normal text-neutral-900 flex items-center m-2 mt-4 justify-center">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("A Simple Cosmetics Comparison Website.")
+                .start();
+            }}
+            options={{
+              delay: 50,
+            }}
+          />
+        </div>
       </div>
     </div>
   );
