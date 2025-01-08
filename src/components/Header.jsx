4bf958b46219cc9879b368e79cc4205fc1638a7e
kevas cole <colekevas@gmail.com>
2025-01-08 15:49:18 +0000

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="font-bebas flex justify-center m-5 pt-5 text-6xl text-fuchsia-900 w-full font-bold">
+        BEAUTY DECODER
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
