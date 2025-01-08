c0248a5b9430d0fbb299d052d39c763aaf771a9e
kevas cole <colekevas@gmail.com>
2025-01-08 15:51:03 +0000

    Added usePathname hook and a rule to ensure it only shows up on the Decoder and About page

--- src/app/client.layout.tsx
+++ src/app/client.layout.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const home = "/";
+  console.log("pathname = ", pathname);
   return (
     <section className="bg-white">
       <NavBar />
+      {pathname !== home && <Header />}
       {children}
     </section>
   );
