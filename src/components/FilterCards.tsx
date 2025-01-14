76da76e2c71b46eb18b6c03379dc8dfe48cc5ecf
kevas cole <colekevas@gmail.com>
2025-01-14 10:19:57 +0000

    Changed the sizes to be only small and medium, deleted some filters that didn't have many products or were not as popular

--- src/components/FilterCards.tsx
+++ src/components/FilterCards.tsx
@@ -6,30 +6,26 @@ import { Card, CardContent } from "@/components/ui/card";
 const filterTags: Array<{
   name: string;
   id: string;
-  size: "small" | "medium" | "large";
+  size: "small" | "medium";
 }> = [
-  { name: "Canadian", id: "Canadian", size: "small" },
   { name: "CertClean", id: "Certified Clean", size: "small" },
-  { name: "cruelty free", id: "Cruelty Free", size: "large" },
+  { name: "cruelty free", id: "Cruelty Free", size: "medium" },
   { name: "oil free", id: "Oil Free", size: "small" },
-  { name: "purpickstClean", id: "Purpicks (Clean Beauty)", size: "small" },
+  { name: "purpickstClean", id: "Purpicks", size: "small" },
   { name: "Chemical Free", id: "Chemical Free", size: "medium" },
   { name: "Dairy Free", id: "Dairy Free", size: "medium" },
   { name: "EWG Verified", id: " EWG Verfied", size: "small" },
   { name: "EcoCert", id: "Eco Certified", size: "small" },
-  { name: "Fair Trade", id: "Fair Trade", size: "large" },
-  { name: "Gluten Free", id: "Gluten Free", size: "large" },
-  { name: "Hypoallergenic", id: "Hypoallergenic", size: "large" },
-  { name: "Natural", id: "Natural", size: "large" },
+  { name: "Fair Trade", id: "Fair Trade", size: "medium" },
+  { name: "Gluten Free", id: "Gluten Free", size: "small" },
+  { name: "Hypoallergenic", id: "Hypoallergenic", size: "medium" },
+  { name: "Natural", id: "Natural", size: "small" },
   { name: "No Talc", id: "No Talc", size: "medium" },
   { name: "Non-GMO", id: "Non GMO", size: "small" },
-  { name: "Organic", id: "Organic", size: "large" },
-  { name: "Peanut Free Product", id: "Peanut Free", size: "small" },
-  { name: "Sugar Free", id: "Sugar Free", size: "small" },
-  { name: "USDA Organic", id: "USDA Organic", size: "small" },
-  { name: "Vegan", id: "Vegan", size: "large" },
+  { name: "Organic", id: "Organic", size: "small" },
+  { name: "Vegan", id: "Vegan", size: "medium" },
   { name: "alc", id: "Alcohol Free", size: "small" },
-  { name: "silicone free", id: "Silicone Free", size: "large" },
+  { name: "silicone free", id: "Silicone Free", size: "medium" },
   { name: "water free", id: "Water Free", size: "medium" },
 ];
 
@@ -40,7 +36,7 @@ const FilterCard = ({
   onToggle,
 }: {
   filter: string;
-  size: "small" | "medium" | "large";
+  size: "small" | "medium";
   isSelected: boolean;
   onToggle: () => void;
 }) => {
@@ -64,13 +60,7 @@ const FilterCard = ({
           isSelected === true ? "text-gray-950 shadow-custom-glow-large" : null
         }`}
       >
-        <p
-          className={`font-medium text-center font-mono
-            ${size === "large" ? "text-xl" : "text-sm"}
-            `}
-        >
-          {filter}
-        </p>
+        <p className="font-medium text-center font-mono">{filter}</p>
       </CardContent>
     </Card>
   );
