e286d2582ffbd79ba8501d0716f7b0fb5a530756
kevas cole <colekevas@gmail.com>
2025-01-14 16:42:08 +0000

    removed top padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h1 className="font-bebas flex justify-center m-5 pt-5 text-6xl text-fuchsia-900 w-full font-bold">
+      <h1 className="font-bebas flex justify-center m-5 text-6xl text-fuchsia-900 w-full font-bold">
         BEAUTY DECODER
       </h1>
     </header>
