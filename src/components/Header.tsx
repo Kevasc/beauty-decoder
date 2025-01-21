b3792e7b5d9f6a3c388e9693c9b79b09a802ee63
kevas cole <colekevas@gmail.com>
2025-01-21 16:15:37 +0000

    Made the header into a link back to the homepage when clicked

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import purpleLogo from "../app/img/purple-logo.png";
 
 const Header = () => {
   return (
     <header className="flex justify-center">
-      <Image
-        src={purpleLogo}
-        width={200}
-        height={150}
-        alt="purple logo"
-      ></Image>
+      <Link href={"/"}>
+        <Image
+          src={purpleLogo}
+          width={200}
+          height={150}
+          alt="purple logo"
+        ></Image>
+      </Link>
     </header>
   );
 };
